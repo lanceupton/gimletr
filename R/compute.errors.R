f939efbabe828be0ded6c5c8bc18e5a269f2d45b
Lance Tyler Upton <lance.upton@gmail.com>
2017-08-10 11:02:26 -0400

    Add group reporting functionality to compute.errors.

--- R/compute.errors.R
+++ R/compute.errors.R
@@ -3,15 +3,17 @@
 #' Compute user tagging errors.
 #'
 #' @param data A data frame containing \code{Id}, \code{TagPre}, and \code{TagPost}.
+#' @param groups Optional. A character vector.
 #'
-#' @return A data frame containing \code{NumEnt}, \code{NumErr}, \code{PerErr}, and \code{Score} for each \code{Id}.
+#' @return A data frame containing \code{NumEnt}, \code{NumErr}, \code{PerErr}, and \code{Score} for each \code{Id}. If \code{groups} is specified, group errors are also computed.
 #'
 #' @examples
 #' compute.errors(data)
+#' compute.errors(data, groupnames)
 #'
 #' @export compute.errors
 
-compute.errors <- function(data) {
+compute.errors <- function(data, groups) {
 
 # Handle Arguments --------------------------------------------------------
 
@@ -37,6 +39,11 @@ compute.errors <- function(data) {
     stringsAsFactors = FALSE
   )
 
+  # Add groups if given
+  if(!missing(groups)) {
+    output$Group <- group
+  }
+
   # Index observations where tag was incorrect
   i.error <- which(data$TagPre != data$TagPost)
 
@@ -53,27 +60,76 @@ compute.errors <- function(data) {
     err <- length(intersect(i.error, i))
 
     # Calculate percent error
-    per <- round((1 - err/ent) * 100, 0)
+    per <- (1 - err/ent) * 100
 
     # Generate a score
     if(ent == 0) {
       score <- "---"
     } else {
-      score <- paste0(ent - err, "/", ent, " | ", per, "%")
+      score <- paste0(ent - err, "/", ent, " | ", round(per, 0), "%")
     }
 
-    # Write numbers
-    output[n,c("NumEnt", "NumErr", "PerErr", "Score")] <- c(ent, err, per, score)
+    # Write numeric
+    output[n,c("NumEnt", "NumErr", "PerErr")] <- c(ent, err, round(per, 2))
+    # Write character
+    output[n,"Score"] <- score
+
+  }
+
+  # Add total
+  ent <- sum(output$NumEnt)
+  err <- sum(output$NumErr)
+  per <- (1 - err/ent) * 100
+  score <- paste0(ent - err, "/", ent, " | ", round(per, 0), "%")
+  # Write numeric
+  output[n+1,c("NumEnt", "NumErr", "PerErr")] <- c(ent, err, round(per, 2))
+  # Write character
+  output[n+1,c("Id","Score")] <- c("total", score)
+
+  # Add groups (if specified)
+  if(!missing(groups)) {
+
+    # Initiate a data frame of data to add
+    add <- data.frame(
+      Id     = "",
+      NumEnt = 0,
+      NumErr = 0,
+      PerErr = 0,
+      Score  = "",
+      stringsAsFactors = FALSE
+    )
+
+    # Vector of unique groups
+    g <- unique(groups)
+
+    # For each group,
+    for(n in 1:length(g)) {
+      # Index group
+      i <- which(groups == g[n])
+      # Get values
+      ent <- sum(output[i,"NumEnt"])
+      err <- sum(output[i,"NumErr"])
+      per <- (1 - err/ent) * 100
+      score <- paste0(ent - err, "/", ent, " | ", round(per, 0), "%")
+
+      # Write numeric
+      add[n,c("NumEnt", "NumErr", "PerErr")] <- c(ent, err, round(per, 2))
+      # Write character
+      add[n,c("Id","Score")] <- c(g[n], score)
+    }
+
+    # Bind to output
+    output <- rbind(output, add)
 
   }
 
   # Format output
   output <- data.frame(
-    Id     = as.character(output$Id),
-    NumEnt = as.integer(output$NumEnt),
-    NumErr = as.integer(output$NumErr),
-    PerErr = as.integer(output$PerErr),
-    Score  = as.character(output$Score),
+    Id     = output$Id,
+    NumEnt = output$NumEnt,
+    NumErr = output$NumErr,
+    PerErr = output$PerErr,
+    Score  = output$Score,
     stringsAsFactors = FALSE
   )
 
