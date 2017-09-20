abc68c2f8014fa97a8f06d57bd6e06830267f1b3
Lance Tyler Upton <lance.upton@gmail.com>
2017-09-19 22:01:03 -0400

    Change argument names to be more useful.

--- R/process.initials.R
+++ R/process.initials.R
@@ -2,45 +2,44 @@
 #'
 #' Verify initials in Gimlet data.
 #'
-#' @param x,id Character vectors.
+#' @param initials,users Character vectors.
 #'
-#' @return \code{TRUE} if all elements in \code{data} are contained within \code{id}. \code{FALSE} and character vector of unmatched elements in there are any.
+#' @return \code{TRUE} if all elements in \code{initials} are contained in \code{users}. \code{FALSE} otherwise.
 #'
 #' @examples
-#' process.ids(data$Initials, idAccepted)
+#' process.ids(initials = data$Initials, users = users)
 #'
-#' @export process.ids
+#' @export process.initials
 
-process.ids <- function(x, id) {
+process.initials <- function(initials, users) {
 
 # Handle Arguments --------------------------------------------------------
 
   # Return error for missing arguments
-  if(missing(x)) {
-    stop("Please specify data.")
+  if(missing(initials)) {
+    stop('Please specify initials.')
   }
   if(missing(id)) {
-    stop("Please specify accepted IDs.")
+    stop('Please specify users.')
   }
-
   # Check argument classes
-  if(!is.character(x)) {
-    stop("x must be character class.")
+  if(!is.character(initials)) {
+    stop('initials must be character class.')
   }
-  if(!is.character(id)) {
-    stop("id must be character class.")
+  if(!is.character(users)) {
+    stop('users must be character class.')
   }
 
 # Function ----------------------------------------------------------------
 
   # Index unmatched elements
-  i <- which(!x %in% id)
+  i <- which(!initials %in% users)
 
   # If there are any,
   if(length(i) > 0) {
 
     # Print them
-    message(cat("Unmatched IDs!", unique(x[i]), sep = "\n"))
+    message(cat('Unmatched IDs!', unique(initials[i]), sep = '\n'))
 
     return(FALSE)
 
