9967db9bf986252f69e3aeac094f48efc19cc6a3
Lance Tyler Upton <lance.upton@gmail.com>
2017-09-20 10:26:07 -0400

    Rename alt to regex for clarity. Correct function of regex.

--- R/process.tags.R
+++ R/process.tags.R
@@ -2,17 +2,16 @@
 #'
 #' Verify tags in Gimlet data.
 #'
-#' @param tags,promoted,alt Character vectors.
+#' @param tags,promoted,regex Character vectors.
 #'
 #' @return A vector of processed tags.
 #'
 #' @examples
 #' process.tags(tags = data$Tags, promoted = c('policy', 'circulation'))
-#' process.tags(tags = data$Tags, promoted = c('policy', 'circulation'), alt = list(policy = c('policies', 'hours'), circulation = c('circ', 'ill', 'borrowing')))
 #'
 #' @export process.tags
 
-process.tags <- function(tags, promoted, alt) {
+process.tags <- function(tags, promoted, regex) {
 
 # Handle Arguments --------------------------------------------------------
 
@@ -30,9 +29,9 @@ process.tags <- function(tags, promoted, alt) {
   if(!is.character(tags)) {
     stop('promoted must be character class.')
   }
-  if(!missing(alt)) {
-    if(!is.list(alt)) {
-      stop('alt must be a list.')
+  if(!missing(regex)) {
+    if(!is.character(regex)) {
+      stop('regex must be character class.')
     }
   }
 
@@ -41,41 +40,23 @@ process.tags <- function(tags, promoted, alt) {
   # Initiate a vector for output
   output <- rep('', length(tags))
 
-  # For each promoted tag,
-  for(n in promoted) {
+  # If regex is not supplied,
+  if(missing(regex)) {
+    # Default
+    regex <- paste0('^', promoted)
+  }
+
+  # For each index in promoted,
+  for(n in 1:length(promoted)) {
 
     # Index first-position matches
-    i <- grep(x = tags, pattern = paste0('^', n), ignore.case = TRUE)
+    i <- grep(x = tags, pattern = regex[n], ignore.case = TRUE)
 
     # If there are any,
     if(length(i) > 0) {
 
       # Assign tag
-      output[i] <- n
-
-    }
-
-  }
-
-  # If alt is supplied,
-  if(!missing(alt)) {
-
-    # Loop through each element in alt
-    for(n in 1:length(alt)) {
-
-      # Generate the regular expression
-      expr <- paste0('^(', paste0(unlist(alt[n]), collapse = '|'), ')')
-
-      # Index first-position matches
-      i <- grep(x = tags, pattern = expr, ignore.case = TRUE)
-
-      # If there are any,
-      if(length(i) > 0) {
-
-        # Assign tag
-        output[i] <- names(alt)[n]
-
-      }
+      output[i] <- promoted[n]
 
     }
 
