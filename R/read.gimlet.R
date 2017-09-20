99ebd64281e61a5a865ea59e1df99a26015d08d6
Lance Tyler Upton <lance.upton@gmail.com>
2017-09-19 22:13:45 -0400

    Add clarifying comments. Suppress rvest messages.

--- R/read.gimlet.R
+++ R/read.gimlet.R
@@ -3,13 +3,13 @@
 #' Download Gimlet data.
 #'
 #' @param site,email,password Strings.
-#' @param start_date,end_date Optional. Objects of class "POSIXt" or "Date".
+#' @param start_date,end_date Optional. Objects of class 'POSIXt' or 'Date'.
 #'
 #' @return A data frame containing the content of a Gimlet data query.
 #'
 #' @examples
-#' read.gimlet("mysite", "e@mail.com", "mypassword")
-#' read.gimlet("mysite", "e@mail.com", "mypassword", as.Date("2010-01-01"), as.Date("2010-01-31"))
+#' read.gimlet('mysite', 'e@mail.com', 'mypassword')
+#' read.gimlet('mysite', 'e@mail.com', 'mypassword', as.Date('2010-01-01'), as.Date('2010-01-31'))
 #'
 #' @export read.gimlet
 
@@ -19,20 +19,20 @@ read.gimlet <- function(site, email, password, start_date, end_date) {
 
   # Return for missing arguments
   if(missing(site)) {
-    stop("Please specify a Gimlet site.")
+    stop('Please specify a Gimlet site.')
   }
   if(missing(email)) {
-    stop("Please specify a login email.")
+    stop('Please specify a login email.')
   }
   if(missing(password)) {
-    stop("Please specify a login password.")
+    stop('Please specify a login password.')
   }
 
   # Handle start_date and end_date
   dates <- missing(start_date) + missing(end_date)
   # One is missing
   if(dates == 1) {
-    stop("Please specify both start_date and end_date, or neither.")
+    stop('Please specify both start_date and end_date, or neither.')
   }
   # Both are missing
   if(dates == 2) {
@@ -41,8 +41,8 @@ read.gimlet <- function(site, email, password, start_date, end_date) {
     end_date <- Sys.Date()
     # Message user
     message(
-      "start_date and end_date missing. Defaulting to last 7 days.\n",
-      start_date, " - ", end_date
+      'start_date and end_date missing. Defaulting to last 7 days.\n',
+      start_date, ' - ', end_date
     )
   }
   # Neither are missing
@@ -52,35 +52,35 @@ read.gimlet <- function(site, email, password, start_date, end_date) {
     end_date <- format(as.Date(end_date))
     # If unordered
     if(start_date > end_date) {
-      stop("start_date must occur before end_date.")
+      stop('start_date must occur before end_date.')
     }
   }
 
   # Check argument classes
   if(!is.character(site) | length(site) != 1) {
-    stop("site must be character class of length 1.")
+    stop('site must be character class of length 1.')
   }
-  if(grepl(pattern = "[^[:alnum:]|-]", x = site, ignore.case = TRUE)) {
-    stop("site must have a valid name.\nAcceptable characters are: a-z, A-Z, 0-9, and -.")
+  if(grepl(pattern = '[^[:alnum:]|-]', x = site, ignore.case = TRUE)) {
+    stop('site must have a valid name.\nAcceptable characters are: a-z, A-Z, 0-9, and -.')
   }
   if(!is.character(email) | length(email) != 1) {
-    stop("email must be character class of length 1.")
+    stop('email must be character class of length 1.')
   }
   if(!is.character(password) | length(password) != 1) {
-    stop("password must be character class of length 1.")
+    stop('password must be character class of length 1.')
   }
 
 # Function ----------------------------------------------------------------
 
   # Generate a Gimlet login URL
-  url <- paste0("https://", site, ".gimlet.us/users/login")
+  url <- paste0('https://', site, '.gimlet.us/users/login')
 
   # Define the html session
   session <- html_session(url)
 
   # Fill out login information
   login <- session %>%
-    html_node("form") %>%
+    html_node('form') %>%
     html_form() %>%
     set_values(
       email = email,
@@ -88,25 +88,32 @@ read.gimlet <- function(site, email, password, start_date, end_date) {
     )
 
   # Submit login information
-  login <- session %>%
-    submit_form(login)
+  login <- suppressMessages(
+    session %>%
+      submit_form(login)
+  )
 
   # Navigate to Reports > Download
   # And fill in report dates
-  data_query <- login %>%
-    follow_link("Reports") %>%
-    follow_link("Download") %>%
-    html_node("form") %>%
-    html_form() %>%
-    set_values(
-      'report[start_date]' = start_date,
-      'report[end_date]' = end_date
-    )
+  data_query <- suppressMessages(
+    login %>%
+      follow_link('Reports') %>%
+      follow_link('Download') %>%
+      html_node('form') %>%
+      html_form() %>%
+      set_values(
+        'report[start_date]' = start_date,
+        'report[end_date]' = end_date
+      )
+  )
 
   # Submit data query
-  response <- session %>% submit_form(data_query)
+  response <- suppressMessages(
+    session %>%
+      submit_form(data_query)
+  )
 
   # Return the parsed response content
-  content(response$response, "parsed")
+  content(response$response, 'parsed')
 
 }
