97d275d08abe3b1b510cbc41172e2b44035b833c
Lance Tyler Upton <lance.upton@gmail.com>
2017-11-01 22:42:44 -0400

    Simplify if() checks and cleaning of initials.

--- R/prep.gimlet.R
+++ R/prep.gimlet.R
@@ -29,8 +29,8 @@ prep.gimlet <- function(data) {
   names <- c('Initials', 'Location', 'Format', 'Asked at', 'Tags', 'Question', 'Answer')
   # Index data names not in names
   i <- which(!names %in% names(data))
-  # If not all names are in the input data,
-  if(length(i) > 0) {
+  # If not all names exist,
+  if(length(i)) {
     stop(paste('Important variables missing from data!', paste(names[i], collapse = '\n'), sep = '\n'))
   }
 
@@ -39,8 +39,7 @@ prep.gimlet <- function(data) {
 
   # Select data
   output <- data.frame(
-    # Remove bad characters from Id
-    initials = gsub(pattern = '[[:punct:]]|[[:space:]]|[[:digit:]]', replacement = '', x = data$Initials),
+    initials = gsub(pattern = '[^[:alpha:]]', replacement = '', x = data$Initials),
     location = data$Location,
     format   = data$Format,
     datetime = data$'Asked at',
@@ -53,23 +52,19 @@ prep.gimlet <- function(data) {
   # For each variable in data,
   for(n in 1:length(output)) {
 
+    # Attach
     x <- output[,n]
-
     # Remove trailing/leading whitespace
     x <- gsub(pattern = '^ +', replacement = '', x = x)
     # Remove duplicate whitespace
     x <- gsub(pattern = ' +', replacement = ' ', x = x)
-
     # Lower case
     x <- tolower(x = x)
-
     # Index blank or NA obs
     i <- union(which(x == ''), which(is.na(x)))
     # Label them with BLANK
-    if(length(i) > 0) {
-      x[i] <- 'BLANK'
-    }
-
+    if(length(i)) {x[i] <- 'BLANK'}
+    # Detach
     output[,n] <- x
 
   }
