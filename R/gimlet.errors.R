bad207aab8efb38ab659b48a6e05710cd5c76cf4
Lance Tyler Upton <lance.upton@gmail.com>
2018-11-12 13:48:34 -0500

    Convert grouping variable to character so a total is easier to append.

--- R/gimlet.errors.R
+++ R/gimlet.errors.R
@@ -56,6 +56,7 @@ gimlet.errors <- function(data, group_by = 'initials') {
   names(errors) <- c(group_by, 'cor', 'err')
 
   # Add total
+  errors[group_by] <- as.character(errors[group_by])
   errors <- rbind(errors, c('Total', sum(errors$cor), sum(errors$err)))
 
   # Add some variables
